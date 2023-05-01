0d41889949b5023c29127d878d42b0856d7403e2
reyshazni <reyshazni@gmail.com>
2023-05-01 16:29:55 +0700

    add gitignore

--- Utils/dbconfig.cs
+++ Utils/dbconfig.cs
@@ -1,9 +1,23 @@
 ﻿using System;
+using DotNetEnv;
+
 namespace kalbestore_be.utils
 {
-	public class dbconfig
+    public class dbconfig
     {
-        public static string ConnectionString { get; } = "Host=localhost;Username=postgres;Password=postgres;Database=kalbe";
+        public static string ConnectionString { get; }
+
+        static dbconfig()
+        {
+            Env.Load(); // Load values from .env file
+
+            // Get values from .env file or use default values
+            string host = Env.GetString("DB_HOST", "localhost");
+            string username = Env.GetString("DB_USERNAME", "postgres");
+            string password = Env.GetString("DB_PASSWORD", "postgres");
+            string database = Env.GetString("DB_DATABASE", "kalbe");
+
+            ConnectionString = $"Host={host};Username={username};Password={password};Database={database}";
+        }
     }
 }
-
